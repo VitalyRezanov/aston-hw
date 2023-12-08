38b9cc981feb668ff90e2033bc00476077f081e1
VitalyRezanov <getahur.rezanov@gmail.com>
2023-12-08 11:59:48 +0300

    Init Lesson_9

--- src/com/aston/Box.java
+++ src/com/aston/Box.java
@@ -2,41 +2,35 @@ package com.aston;
 
 import java.util.List;
 
-public class Box {
-    private List<Fruit> apples;
-    private List<Fruit> oranges;
+public class Box<T extends Fruit> {
+    private List<T> fruits;
 
-    public Box(List<Fruit> apples, List<Fruit> oranges) {
-        this.apples = apples;
-        this.oranges = oranges;
+    public Box(List<T> fruits) {
+        this.fruits = fruits;
     }
 
-    public List<Fruit> getApples() {
-        return apples;
+    public Box() {
     }
 
-    public List<Fruit> getOranges() {
-        return oranges;
+    public List<T> getFruits() {
+        return fruits;
     }
 
-    public void add(Fruit fruit) {
-        if (fruit instanceof Apple)
-            apples.add(fruit);
+    public void add(T fruit) {
+        if (this.fruits.get(0).getClass() == fruit.getClass())
+            fruits.add(fruit);
         else
-            oranges.add(fruit);
+            System.out.println("Этот фрукт нельзя положить в коробку");
     }
     public double getWeight() {
-
-        return apples.stream().mapToDouble(Fruit::getFruitWeight).sum()
-                + oranges.stream().mapToDouble(Fruit::getFruitWeight).sum();
+        return fruits.stream().mapToDouble(Fruit::getFruitWeight).sum();
     }
     public boolean compare(Box box) {
         return (this.getWeight() - box.getWeight()) == 0;
     }
-    public Box getBox() {
-        Box box = new Box(this.apples, this.oranges);
-        this.apples = null;
-        this.oranges = null;
+    public Box<T> getBox() {
+        Box<T> box = new Box<>(fruits);
+        this.fruits = null;
         return box;
     }
 }
--- src/com/aston/Main.java
+++ src/com/aston/Main.java
@@ -1,7 +1,19 @@
 package com.aston;
 
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Box<Apple> apples = new Box<>(List.of(new Apple(), new Apple(), new Apple()));
+        Box<Orange> oranges = new Box<>(List.of(new Orange(), new Orange()));
+
+        System.out.println(apples.getWeight());
+        Box<Apple> box1 = new Box<>(List.of(new Apple(), new Apple()));
+        Box<Apple> box2 = box1.getBox();
+        System.out.println("box1:" + box1.getFruits() + " box2:" + box2.getFruits());
+
+        System.out.println(oranges.compare(apples));
+
     }
 }
